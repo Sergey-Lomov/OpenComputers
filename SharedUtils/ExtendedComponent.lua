54fcc80502021edc2e54851cf26f34bb8d862d9b
Sergey Lomov <SSpirit10000@gmail.com>
2021-12-09 03:30:29 +0200

    Added safePrimary to component

--- SharedUtils/ExtendedComponent.lua
+++ SharedUtils/ExtendedComponent.lua
@@ -1,13 +1,21 @@
-component = require ("component")
-utils = require ("utils")
+local component = require ("component")
+local utils = require ("utils")
 
-function component:shortProxy(shortId)
+function component.shortProxy(shortId)
 	local id = self.get(shortId)
 	if id == nil then
 		utils:showError("Can't find full id for short: " .. shortId)
 		return nil
 	end
 	return self.proxy(id)
+end
+
+function component.safePrimary(type)
+	if next(component.list(type)) ~= nil then
+		return component[type]
+	else
+		return nil
+	end
 end
 
 return component
