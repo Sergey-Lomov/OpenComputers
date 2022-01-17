1beee5e7f570e779d26d38d49d59f9d4de996730
Sergey Lomov <SSpirit10000@gmail.com>
2022-01-17 19:19:54 +0200

    Add to builder possibility to load resources automatically

--- Builder/Builder.lua
+++ Builder/Builder.lua
@@ -1,16 +1,18 @@
 local utils = require ("utils")
 local component = require("component")
+local sides = require("sides")
 local icExtender = require("ic_extender")
+
+local Errors = {
+	missedSchemeOnResourcesLoading = "Попытка загрузить ресуры до выбора схемы",
+	missedResourcesBase = "Не обнаружена ресурсная база",
+	missedResources = "Недостаточно ресурсов для строительства"
+}
 
 local builder = {
 	navigator = require("navigator"),
 	inventory = component.inventory_controller,
-	schema = {
-		emptyName = "none",
-		size = {x = 0, z = 0},
-		codes = {},
-		layers = {}
-	}
+	schema = nil
 }
 
 -- Private methods
@@ -22,10 +24,10 @@ function builder:getNameInPoint(x, y, z)
 	return builder.schema.codes[code]
 end
 
-function builder:verifyInventory()
-	local counts = self.inventory:getCounts()
-	
-	local required = {}
+function builder:requiredResources()
+	if self.schema == nil then return {} end
+
+	local requiredByCodes = {}
 	local layers = self.schema.layers
 	for layerIndex = 1, #layers do
 		local layer = layers[layerIndex]
@@ -33,21 +35,35 @@ function builder:verifyInventory()
 			local line = layer[lineIndex]
 			for codeIndex = 1, #line do
 				local code = line:sub(codeIndex, codeIndex)
-				required[code] = (required[code] or 0) + 1
+				requiredByCodes[code] = (requiredByCodes[code] or 0) + 1
 			end
 		end
 	end
 
-	local haveMaterials = true
-	for code, count in pairs(required) do
+	local requiredByNames = {}
+	for code, count in pairs(requiredByCodes) do
 		local name = self.schema.codes[code]
-		local diff = count - (counts[name] or 0)
-		if diff > 0 and name ~= self.schema.emptyName then
-			print("  Need " .. tostring(diff) .. " of " .. name)
-			haveMaterials = false
+		if name ~= self.schema.emptyName then
+			requiredByNames[name] = count
 		end
 	end
-	return haveMaterials
+
+	return requiredByNames
+end
+
+function builder:verifyInventory()
+	local counts = self.inventory:getCounts()
+	local required = self:requiredResources()
+
+	local enoughtResources = true
+	for name, count in pairs(required) do
+		local diff = count - (counts[name] or 0)
+		if diff > 0 then
+			print("  Need " .. tostring(diff) .. " of " .. name)
+			enoughtResources = false
+		end
+	end
+	return enoughtResources
 end
 
 -- Public methods
@@ -114,6 +130,63 @@ end
 function builder:init()
 	icExtender:extend(self.inventory)
 	self.navigator:restoreState()
+end
+
+function builder:loadResources(verbose)
+	verbose = verbose or true
+
+	if self.schema == nil then
+		if verbose then print(Errors.missedSchemeOnResourcesLoading) end
+		return false, Errors.missedSchemeOnResourcesLoading
+	end
+
+	local side = sides.front
+
+	local inventorySize = self.inventory.getInventorySize(side)
+	if type(inventorySize) ~= "number" then
+		if verbose then print(Errors.missedResourcesBase) end
+		return false, Errors.missedResourcesBase
+	end
+
+	local resources = self:requiredResources()
+	local alreadyHave = self.inventory:getCounts()
+	for name, count in pairs(alreadyHave) do
+		local required = resources[name]
+		if required ~= nil then
+			resources[name] = required - count
+		end
+	end
+
+	for i = 1, inventorySize, 1 do
+		local stack = self.inventory.getStackInSlot(side, i)
+		if stack == nil then goto continue end
+		
+		local required = resources[stack.name]
+		if required == nil or required == 0 then goto continue end
+
+		local suckSize = math.min(required, stack.size)
+		self.inventory.suckFromSlot(side, i, suckSize)
+		resources[stack.name] = required - suckSize
+
+		::continue::
+	end
+
+	local readyToBuild = true
+	for resource, required in pairs(resources) do
+		if required ~= 0 then
+			if verbose then
+				if readyToBuild then print(Errors.missedResources) end
+				print("\t" .. resource .. "\t" .. required)
+			end
+			readyToBuild = false
+		end
+	end
+
+	if not readyToBuild then 
+		return false, Errors.missedResources
+	end
+
+	return true
 end
 
 builder:init()
