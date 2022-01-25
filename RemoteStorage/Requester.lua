46a59f2251b83f27fcb3c97994ed9a02ae232d93
Sergey Lomov <SSpirit10000@gmail.com>
2022-01-25 18:26:28 +0200

    Added missed data check to data requester

--- RemoteStorage/Requester.lua
+++ RemoteStorage/Requester.lua
@@ -12,7 +12,12 @@ function requestData()
 		print(tostring(args[1]) .. " " .. tostring(args[2]) .. " " .. tostring(args[3]) .. " " .. tostring(args[4]) .. " " .. tostring(args[5]))
 
 		if args[1] == "modem_message" and args[4] == port and args[5] == dataId then
-			x,y = load(args[#args])()
+			local data = args[#args]
+			if data == nil or data == "" then 
+				computer.beep(1200, 3)
+				return
+			end
+			x,y = load(data)()
 			-- Here should be buisnes logic
 			computer.beep(300, 3)
 			modem.close(port)
