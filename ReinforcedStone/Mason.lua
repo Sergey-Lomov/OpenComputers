0faa009fcd977f4eafa14106bd490df798e8f847
Sergey Lomov <SSpirit10000@gmail.com>
2022-01-28 02:58:26 +0200

    Mason code refactored. Status handling improved.

--- ReinforcedStone/Mason.lua
+++ ReinforcedStone/Mason.lua
@@ -1,66 +1,83 @@
 require("utils")
 
-sides = require("sides")
-robot = require("robot")
-component = require("component")
-icExtender = require("ic_extender")
+local sides = require("sides")
+local robot = require("robot")
+local component = require("component")
+local icExtender = require("ic_extender")
+local status = require("status_client")
 
-ic = component.inventory_controller
-icExtender:extend(ic)
-rs = component.redstone
+local inventory = component.inventory_controller
+icExtender:extend(inventory)
 
-configFile = "config"
-waitingStep = 2
+local rs = component.redstone
 
-mason = {
+local configFile = "config"
+local waitingStep = 2
+
+local Id = {
+	sand = "minecraft:sand",
+	sprayer = "IC2:itemFoamSprayer",
+	scaffold = "IC2:blockIronScaffold",
+	drill = "GraviSuite:advDDrill",
+	stone = "IC2:blockAlloy",
+}
+
+local mason = {
 	config = {},
 	iteration = 1
 }
 
 function mason:init()
 	self.config = utils:loadFrom(configFile)
+
+	status.pingId = inventory.address
+	status.pingTitle = self.config.pingTitle
+	status.pingAllowableDelay = self.config.pingAllowableDelay
+	status.statusStrength = self.config.statusStrength
 end
 
 function mason:useSand(name)
-	self:prepareAndSelect("minecraft:sand", true)
-	ic.equip()
+	self:prepareAndSelect(Id.sand, true)
+	inventory.equip()
 	robot.use()
 end
 
 function mason:placeSprayer(name)
-	self:prepareAndSelect("IC2:itemFoamSprayer", false)
+	self:prepareAndSelect(Id.sprayer, false)
 	robot.place() -- Yep, we have possibility to place sprayer without equip
 end
 
 function mason:placeScaffold(name)
-	self:prepareAndSelect("IC2:blockIronScaffold", true)
+	self:prepareAndSelect(Id.scaffold, true)
 	robot.place()
 end
 
 function mason:swingDrill(name)
-	self:prepareAndSelect("GraviSuite:advDDrill", false)
-	ic.equip()
+	self:prepareAndSelect(Id.drill, false)
+	inventory.equip()
 	robot.swing()
 end
 
 function mason:prepareAndSelect(name, tryToSuck)
 	local reported = false
-	local slot = ic:firstInternalSlotWhere("name", name)
+	local slot = inventory:firstInternalSlotWhere("name", name)
 	while slot == nil do
+		status:sendPing()
+
 		if tryToSuck then
 			if not reported then
 				print("Try to suck: " .. name)
 				reported = true
 			end
 
-			local sourceSlot = ic:firstSlotWhere(self.config.inSide, "name", name)
+			local sourceSlot = inventory:firstSlotWhere(self.config.inSide, "name", name)
 			if sourceSlot ~= nil then
-				slot = ic:firstInternalEmptySlot()
+				slot = inventory:firstInternalEmptySlot()
 				robot.select(slot)
-				ic.suckFromSlot(self.config.inSide, sourceSlot)
+				inventory.suckFromSlot(self.config.inSide, sourceSlot)
 			end
 		else
-			slot = ic:firstInternalSlotWhere("name", name)
+			slot = inventory:firstInternalSlotWhere("name", name)
 		end
 	end
 
@@ -68,19 +85,19 @@ function mason:prepareAndSelect(name, tryToSuck)
 end
 
 function mason:pushOutDrop()
-	local slot = ic:firstInternalSlotWhere("name", "IC2:blockAlloy")
+	local slot = inventory:firstInternalSlotWhere("name", Id.stone)
 	while slot ~= nil do
-		local targetSlot = ic:firstEmptySlot(self.config.outSide)
+		local targetSlot = inventory:firstEmptySlot(self.config.outSide)
 		robot.select(slot)
-		ic.dropIntoSlot(self.config.outSide, targetSlot)
-		slot = ic:firstInternalSlotWhere("name", "IC2:blockAlloy")
+		inventory.dropIntoSlot(self.config.outSide, targetSlot)
+		slot = inventory:firstInternalSlotWhere("name", Id.stone)
 	end
 end
 
 function mason:pushOutTools()
-	local emptySlot = ic:firstInternalEmptySlot()
+	local emptySlot = inventory:firstInternalEmptySlot()
 	robot.select(emptySlot)
-	ic.equip()
+	inventory.equip()
 
 	rs.setOutput(self.config.toolsSignalSide, 15)
 	os.sleep(self.config.toolsOutDelay)
@@ -90,7 +107,11 @@ function mason:pushOutTools()
 end
 
 function mason:start()
+	status:sendPing(true)
+
 	while true do
+		status:sendPing()
+
 		self:placeScaffold()
 		self:placeSprayer()
 		self:useSand()
