2ed735399a6fd569b2d1e69c0b5a5b6b38097cef
Sergey Lomov <SSpirit10000@gmail.com>
2022-02-06 01:49:09 +0200

    Updated recipes for galaxy assemblers and induction furnance

--- RemoteStorage/Requester.lua
+++ RemoteStorage/Requester.lua
@@ -1,20 +1,18 @@
 local port = 2327
 local dataId = "induction_recipes"
 local modem = component.proxy(component.list("modem")())
+if modem == nil then computer.beep(900, 1) return end
+
 function requestData()
-	if modem == nil then computer.beep(900, 3) return end
 	modem.open(port)
 
 	while true do
 		modem.broadcast(port, dataId)
 		local args = {computer.pullSignal(10)}
-		print("Pulled signal")
-		print(tostring(args[1]) .. " " .. tostring(args[2]) .. " " .. tostring(args[3]) .. " " .. tostring(args[4]) .. " " .. tostring(args[5]))
-
 		if args[1] == "modem_message" and args[4] == port and args[5] == dataId then
 			local data = args[#args]
 			if data == nil or data == "" then 
-				computer.beep(1200, 3)
+				computer.beep(1200, 0.25)
 				return
 			end
 			x,y = load(data)()
